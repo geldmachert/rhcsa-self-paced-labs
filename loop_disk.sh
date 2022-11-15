3cb67ff89e5f0fa95d5d831a7ffd4c2326f61f46
geldmachert <91529248+geldmachert@users.noreply.github.com>
2022-11-16 00:40:19 +0800

    Update loop_disk.sh

--- loop_disk.sh
+++ loop_disk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch /example.img
-dd if=/dev/zero of=/example.img bs=1M count=1000 status=progress
+dd if=/dev/zero of=/example.img bs=1M count=2000 status=progress
 losetup /dev/loop0 /example.img
